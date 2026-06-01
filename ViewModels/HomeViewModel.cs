07c4ff944004f78b4f00117f9d16cec6a2c3e4f2
Zain Farhan <zainfarhan.work@gmail.com>
2026-06-01 19:37:08 +0500

    Basic frontend

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,6 +1,22 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace Quill.ViewModels;
 
 public partial class HomeViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to the Home Page!";
+    // This is redundant and only acts as a placeholder rn
+    [ObservableProperty]
+    private string debug  = "Debug Statement!";
+    
+    // Will change from string to a proper datatype later
+    private ObservableCollection<string> RecentDocuments { get; } = new() { "Doc 1", "Doc 2", "Doc 3" };
+
+    [RelayCommand]
+    public void NewFile()
+    {
+        Debug = Debug == "Click 1" ? "Click 2" : "Click 1";
+    }
+    
 }

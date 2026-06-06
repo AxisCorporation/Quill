fc90290cfdc050d9cc8affc1f5d117695765d166
Isaacashar108 <isaacashar108@gmail.com>
2026-06-05 19:15:11 -0500

    Added stringbuilder variable to store text, get method to read text, add character method, and remove character method(backspace).

--- Models/TextDocument.cs
+++ Models/TextDocument.cs
@@ -6,5 +6,23 @@ namespace Quill.Models
 {
     internal class TextDocument
     {
+        private StringBuilder text = new StringBuilder();
+
+        public string Text
+        {
+            get { return text.ToString(); }
+        }
+        public void AddCharacter(char c)
+        {
+            text.Append(c);
+        }
+
+        public void RemoveCharacter()
+        {
+            if (text.Length == 0)
+                return;
+
+            text.Length--;
+        }
     }
 }

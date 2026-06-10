bc3b999d1d650af92d7384da9be7de528b0253ba
Mohammad Daud Khawaja <daud.khawaja@outlook.com>
2026-06-10 17:10:22 -0500

    Implemented buttons to go to home

--- ViewModels/EditorViewModel.cs
+++ ViewModels/EditorViewModel.cs
@@ -1,6 +1,12 @@
+using CommunityToolkit.Mvvm.Input;
+
 namespace Quill.ViewModels;
 
-public class EditorViewModel : ViewModelBase
+public partial class EditorViewModel : ViewModelBase
 {
-    
+    [RelayCommand]
+    private void GoToHome()
+    {
+        MainWindowViewModel.Navigate(new HomeViewModel());
+    }
 }
